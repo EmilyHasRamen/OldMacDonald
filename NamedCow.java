cc01a6d918801d587276b1726f8b0622b7ccf370
EmilyHasRamen <emily.has.ramen@gmail.com>
2016-10-13 14:23:54 -0700

    10-13-16

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,15 @@
-class NamedCow
+class NamedCow extends Cow
 {
-
+	private String myName;
+	public NamedCow(String animalName)
+	{
+		myName = animalName
+	}
+	 public NamedCow()     {         
+         myName = "unknown";           
+     }      
+	public getName()
+	{
+		return 
+	}
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -2,5 +2,7 @@
 void setup()
 {
 	//your variable declarations here
+	Cow c = new Cow("cow", "moo");   
+    System.out.println(c.getType() + " goes " + c.getSound());    
 }
 
