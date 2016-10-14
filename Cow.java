c568964366ddcb2b2d0e56cde44d47b62181621f
EmilyHasRamen <emily.has.ramen@gmail.com>
2016-10-14 14:05:12 -0700

    Much Dogo

--- Cow.java
+++ Cow.java
@@ -4,16 +4,16 @@ class Cow implements Animal
      protected String myType;     
      protected String mySound;      
      public Cow(String type, String sound)     {         
-//System.out.println("I'm in Cow(String type, String sound)");
+
          myType = type;         
          mySound = sound;     
      }     
 
      public Cow()     {         
-//System.out.println("I'm in Cow()");
+
          myType = "unknown";         
          mySound = "unknown";     
-//System.out.println("myType="+myType+", mySound="+mySound);
+
      }      
 
      public String getSound(){return mySound;}     
--- Farm.java
+++ Farm.java
@@ -13,16 +13,6 @@ class Farm
       }       
          System.out.println( "The cow is known as " + ((NamedCow)aBunchOfAnimals[0]).getName() );    
     } 
-/*
-     private Animal[] aBunchOfAnimals = new Animal[3];    
-   public Farm()     {       
-      aBunchOfAnimals[0] = new Cow("cow","moo");           
-      aBunchOfAnimals[1] = new Chick("chick","cluck");       
-      aBunchOfAnimals[2] = new Pig("pig","oink");    }         
-   public void animalSounds()    {       
-     for (int nI=0; nI < aBunchOfAnimals.length; nI++)       {          
-       System.out.println( aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound());       
-      }    
-    } 
-*/
+
+    
 }
